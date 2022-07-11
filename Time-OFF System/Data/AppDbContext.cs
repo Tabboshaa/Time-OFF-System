02ede3e3b10a19472d50385fa92aaf046706a45f
Abdalaziz Tabbosha <abdalaziztabbosha@gmail.com>
2022-07-11 19:28:14 +0200

    Add project files.

---
+++ Time-OFF System/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Time_OFF_System.Models;
+
+namespace Time_OFF_System.Data
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+
+    }
+}
---
+++ Time-OFF System/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Time_OFF_System.Models
+{
+    public class AppUser:IdentityUser
+    {
+
+    }
+}
