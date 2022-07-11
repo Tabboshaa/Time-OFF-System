938f8c0ab1033dfccf608edf7f1b1fc2bbecb90a
Abdalaziz Tabbosha <abdalaziztabbosha@gmail.com>
2022-07-11 22:05:55 +0200

    Adding Account Controller

---
+++ Time-OFF System/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Time_OFF_System.Data;
+using Time_OFF_System.Models;
+
+namespace Time_OFF_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly AppDbContext context;
+
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            AppDbContext context)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
